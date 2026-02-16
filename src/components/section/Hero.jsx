0d1287a54c35b73c88e3f3079d0e6b5772d0180c
muhammadidzoha <muhammadidzoha@gmail.com>
2026-02-16 10:55:52 +0700

    [UPDATE] Remove OurPartner.js Move to Hero.jsx

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -21,7 +21,57 @@ import { Badge } from "../../components/ui/badge";
 import { companyInfo } from "@/data/companyData";
 import { stats } from "@/data/statsData";
 import Marquee from "react-fast-marquee";
-import { clients } from "../../data/ourPartner";
+import inno from "@/assets/images/inno.jpeg";
+import adhibeton from "@/assets/images/adhibeton.jpeg";
+import aml from "@/assets/images/aml.png";
+import krakatau from "@/assets/images/krakatau.png";
+import mahugi from "@/assets/images/mahugi.jpeg";
+import sharon from "@/assets/images/sharon.png";
+import sierra from "@/assets/images/sierra.png";
+import ss from "@/assets/images/ss.png";
+
+const clients = [
+  {
+    id: 1,
+    name: "PT Inno Jaya Tekstil",
+    logo: inno
+  },
+  {
+    id: 2,
+    name: "Adhi Persada Beton",
+    logo: adhibeton
+  },
+  {
+    id: 3,
+    name: "PT Anugerah Megah Lestari Tekstil",
+    logo: aml
+  },
+  {
+    id: 4,
+    name: "PT Krakatau Steel",
+    logo: krakatau
+  },
+  {
+    id: 5,
+    name: "PT Mahugi Jaya Sejahtera",
+    logo: mahugi
+  },
+  {
+    id: 6,
+    name: "Sharon Bakery",
+    logo: sharon
+  },
+  {
+    id: 7,
+    name: "PT Jayamas Dwi Perkasa",
+    logo: sierra
+  },
+  {
+    id: 8,
+    name: "Sinar Sari Sejati",
+    logo: ss
+  },
+];
 
 const iconMap = {
   Settings,
