33a3e7e85ecf1f9186e2d13a7182e2026f9eefd7
muhammadidzoha <muhammadidzoha@gmail.com>
2026-02-16 10:45:06 +0700

    [UPDATE] Import Logo OurPartner.js

--- src/data/ourPartner.js
+++ src/data/ourPartner.js
@@ -1,42 +1,51 @@
+import inno from "@/assets/images/inno.jpeg";
+import adhibeton from "@/assets/images/adhibeton.jpeg";
+import aml from "@/assets/images/aml.png";
+import krakatau from "@/assets/images/krakatau.png";
+import mahugi from "@/assets/images/mahugi.jpeg";
+import sharon from "@/assets/images/sharon.png";
+import sierra from "@/assets/images/sierra.png";
+import ss from "@/assets/images/ss.png";
+
 export const clients = [
   {
     id: 1,
     name: "PT Inno Jaya Tekstil",
-    logo: "src/assets/images/inno.jpeg",
+    logo: inno
   },
   {
     id: 2,
     name: "Adhi Persada Beton",
-    logo: "src/assets/images/adhibeton.jpeg",
+    logo: adhibeton
   },
   {
     id: 3,
     name: "PT Anugerah Megah Lestari Tekstil",
-    logo: "src/assets/images/aml.png",
+    logo: aml
   },
   {
     id: 4,
     name: "PT Krakatau Steel",
-    logo: "src/assets/images/krakatau.png",
+    logo: krakatau
   },
   {
     id: 5,
     name: "PT Mahugi Jaya Sejahtera",
-    logo: "src/assets/images/mahugi.jpeg",
+    logo: mahugi
   },
   {
     id: 6,
     name: "Sharon Bakery",
-    logo: "src/assets/images/sharon.png",
+    logo: sharon
   },
   {
     id: 7,
     name: "PT Jayamas Dwi Perkasa",
-    logo: "src/assets/images/sierra.png",
+    logo: sierra
   },
   {
     id: 8,
     name: "Sinar Sari Sejati",
-    logo: "src/assets/images/ss.png",
+    logo: ss
   },
 ];
